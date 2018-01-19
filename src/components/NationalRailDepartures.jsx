773037ec2e4fdeca76d29ebc9bbf835afbec84b8
Jon Linnell <linnell.jon@gmail.com>
2018-01-19 21:50:57 +0000

    Styling and Webpack tweaks

--- src/components/NationalRailDepartures.jsx
+++ src/components/NationalRailDepartures.jsx
@@ -10,24 +10,23 @@ export default class Departures extends Component {
     super(props)
     this.state = {
       departures: [],
-      error: false,
       loading: false,
-      title: props.title,
-      station: props.station,
+      stationName: props.stationName,
+      stationCode: props.stationCode,
       callingPointCode: props.callingPointCode,
       callingPointName: props.callingPointName
     }
   }
 
   loadData() {
-    if (this.state.station !== null) {
+    if (this.state.stationCode !== null) {
       const departures = []
       this.setState({ loading: true })
-      rail.getDepartureBoardWithDetails(this.state.station, {
+      rail.getDepartureBoardWithDetails(this.state.stationCode, {
         destination: this.state.callingPointCode
       }, (err, response) => {
         if (err) {
-          this.setState({ error: true, loading: false })
+          this.setState({ loading: false })
         } else {
           response.trainServices.map((departure, i) =>
             departures.push(<TrainDepartureInfo key={i} departure={departure} />))
@@ -44,8 +43,8 @@ export default class Departures extends Component {
   componentWillReceiveProps(nextProps) {
     this.setState({
       error: false,
-      title: nextProps.title,
-      station: nextProps.station,
+      stationName: nextProps.stationName,
+      stationCode: nextProps.stationCode,
       callingPointCode: nextProps.callingPointCode,
       callingPointName: nextProps.callingPointName
     }, () => {
@@ -69,24 +68,23 @@ export default class Departures extends Component {
       <div id='accordion' className='tablist'>
         <div className='list-group-item departure-board-header'>
           <div role="tab">
-            <h4 className="mb-0">
-              <div className='m-0 p-2 pb-0'>
-                <h3 className=''>{this.state.title}</h3>
-                <p className='departure-board-header-subtitle m-0'>Departures from this station
-                {
-                  this.state.callingPointName
-                  ? ` calling at ${this.state.callingPointName}.`
-                  : '.'
-                }
-                </p>
-              </div>
-            </h4>
+            <div className='m-0 p-2 pb-0'>
+              <h3>{this.state.stationName}</h3>
+              <p className='departure-board-header-subtitle m-0'>Next trains from this station
+              {
+                this.state.callingPointName
+                ? ` calling at ${this.state.callingPointName}.`
+                : '.'
+              }
+              </p>
+            </div>
           </div>
         </div>
-
-            {this.state.loading === true
-              ? <div className='spinner'><div className="bounce1"></div><div className="bounce2"></div><div className="bounce3"></div></div>
-              : this.state.departures}
+          {
+            this.state.loading === true
+              ? <div className='spinner my-5'><div className="bounce1"></div><div className="bounce2"></div><div className="bounce3"></div></div>
+              : this.state.departures
+          }
       </div>
     )
   }
--- src/components/Rail.jsx
+++ src/components/Rail.jsx
@@ -26,38 +26,42 @@ export default class Rail extends Component {
           <div className='col-xs-12 col-lg-6 offset-lg-3'>
             <div className='jumbotron jumbotron-fluid'>
               <div className='container'>
-                <h1 className='display-4'>National Rail Departures</h1>
-                <p className='lead'>The next trains departing from this station.</p>
+                <h1 className='display-4'>Train Times</h1>
+                <h3>National Rail Services</h3>
+                <p>This page lists the next trains to leave a National Rail
+                train station within the next two hours.</p>
+                <p>You can filter trains by stations they stop at. Tap the
+                More... icon to see all calling points.</p>
+                <form>
+                  <div className='form-row'>
+                    <div className='form-group col-lg col-xs-12'>
+                      <label htmlFor='stationSelector'>Station Name</label>
+                      <Typeahead
+                        labelKey='name'
+                        ref='station'
+                        multiple={false}
+                        options={railStations}
+                        onChange={station => this.setState({ station: station[0] })}
+                        onFocus={this.handleClearCallingPoint}
+                      />
+                    </div>
+                  </div>
+                  <div className='form-row'>
+                    <div className='form-group col-lg col-xs-12'>
+                      <label htmlFor='stationSelector'>Filter (trains calling at...)</label>
+                      <Typeahead
+                        labelKey='name'
+                        ref='callingPoint'
+                        multiple={false}
+                        options={railStations}
+                        disabled={!this.state.station}
+                        onChange={station => this.setState({ callingPoint: station[0] })}
+                        autocomplete={false}
+                      />
+                    </div>
+                  </div>
+                </form>
               </div>
-              <form>
-                <div className='form-row py-1 px-3'>
-                  <div className='form-group col-lg col-xs-12'>
-                    <label htmlFor='stationSelector'>Station Name</label>
-                    <Typeahead
-                      labelKey='name'
-                      ref='station'
-                      multiple={false}
-                      options={railStations}
-                      onChange={station => this.setState({ station: station[0] })}
-                      onFocus={this.handleClearCallingPoint}
-                    />
-                  </div>
-                </div>
-                <div className='form-row py-1 px-3'>
-                  <div className='form-group col-lg col-xs-12'>
-                    <label htmlFor='stationSelector'>Filter (trains calling at...)</label>
-                    <Typeahead
-                      labelKey='name'
-                      ref='callingPoint'
-                      multiple={false}
-                      options={railStations}
-                      disabled={!this.state.station}
-                      onChange={station => this.setState({ callingPoint: station[0] })}
-                      autocomplete={false}
-                    />
-                  </div>
-                </div>
-              </form>
             </div>
           </div>
         </div>
@@ -66,9 +70,8 @@ export default class Rail extends Component {
             {
               this.state.station
                 ? <Departures
-                    title={`${this.state.station.name}`}
-                    subtitle={this.state.callingPoint ? `Trains calling at ${this.state.callingPoint.name}` : 'All departures'}
-                    station={this.state.station.code}
+                    stationName={`${this.state.station.name}`}
+                    stationCode={this.state.station.code}
                     callingPointName={this.state.callingPoint ? this.state.callingPoint.name : null}
                     callingPointCode={this.state.callingPoint ? this.state.callingPoint.code : null}
                     limit={10}
